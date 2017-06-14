5bd4be8e4d37f76b9500ed75837382a99e7f2815
wolf <vivanovic@gmail.com>
2017-06-14 14:08:12 +0200

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
 var builder = require('botbuilder');
 var restify = require('restify');
 
-var connector = new builder.ChatConnector();
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 var bot = new builder.UniversalBot(connector);
 bot.dialog('/', [
 function(session) {
@@ -11,7 +14,8 @@ function(session, results) {
     session.send('hello, ' + results.response);
 }
 ]);
-
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c413b2ef-382c-45bd-8ff0-f76d60e2a821?subscription-key=6d0966209c6e4f6b835ce34492f3e6d9';
+bot.recognizer(new builder.LuisRecognizer(model));
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 9000, function(){
 console.log('%s listening to %s', server.name, server.url);
